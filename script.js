721caaf3b70bc089b8df3047fcd2b42860e28d29
RahulSonwal <rahulsonwal3@gmail.com>
2023-05-04 23:49:56 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,66 @@
+const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+function age_calculate() {
+    let today = new Date();
+    let inputDate = new Date(document.getElementById("date-input").value);
+    let birthMonth, birthDate, birthYear;
+    let birthDetails = {
+        date: inputDate.getDate(),
+        month: inputDate.getMonth() + 1,
+        year: inputDate.getFullYear()
+    }
+    let currentYear = today.getFullYear();
+    let currentMonth = today.getMonth() + 1;
+    let currentDate = today.getDate();
+
+    checker(currentYear);
+
+    if (
+        birthDetails.year > currentYear ||
+        (birthDetails.month > currentMonth && birthDetails.year == currentYear) ||
+        (birthDetails.date > currentDate && birthDetails.month == currentMonth && birthDetails.year == currentYear)
+    ) {
+        alert("Not Born Yet");
+        display_message("-", "-", "-");
+        return;
+    }
+
+    birthYear = currentYear - birthDetails.year;
+
+    if (currentMonth >= birthDetails.month) {
+        birthMonth = currentMonth - birthDetails.month;
+    }
+    else {
+        birthYear--;
+        birthMonth = 12 + currentMonth - birthDetails.month;
+    }
+
+    if (currentDate >= birthDetails.date) {
+        birthDate = currentDate - birthDetails.date;
+    }
+    else {
+        birthMonth--;
+        let days = months[currentMonth - 2];
+        birthDate = days + currentDate - birthDetails.date;
+        if (birthMonth < 0) {
+            birthMonth = 11;
+            birthYear--;
+        }
+    }
+    display_message(birthDate, birthMonth, birthYear);
+}
+
+function display_message(bDate, bMonth, bYear) {
+    document.getElementById("years").textContent = bYear;
+    document.getElementById("months").textContent = bMonth;
+    document.getElementById("days").textContent = bDate;
+}
+
+function checker(year) {
+    if (year % 4 == 0 || (year % 100 == 0 && year % 400 == 0)) {
+        months[1] = 29;
+    }
+    else {
+        months[1] = 28;
+    }
+}
